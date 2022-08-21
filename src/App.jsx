bca9a060075b155710992f644207d0dfd07f9094
Gingaling <nycgingsf@hotmail.com>
2022-08-21 14:49:33 -0700

    Committing Navigation files

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,24 @@ import React, { useState } from 'react';
 import data from './data.json';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import CarouselContainer from './CarouselContainer';
+import Navigation from './Navigation';
+import Container from 'react-bootstrap/Container';
 
 function App() {
 	const [searchOptions, setSearchOptions] = useState({
 		key: process.env.REACT_APP_RIJKS_KEY,
 		url: 'https://www.rijksmuseum.nl/api/en/',
-		numberOfResults: 15
+		numberOfResults: 50
 	});
 	return (
-		<main>
-			<CarouselContainer data={data} />
-		</main>
+		<Container>
+			<Navigation />
+			<main>
+				<Routes>
+					<Route path="/home" element={<CarouselContainer data={data} />} />
+				</Routes>
+			</main>
+		</Container>
 	);
 }
 
